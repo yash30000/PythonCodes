5789792a1cf050dda16140b2ff4773a0ed452ef2
Yashraj Wakadkar <136210721+yash30000@users.noreply.github.com>
2025-12-08 12:38:46 +0000

    Add

---
+++ Alarm Clock/Time.py
@@ -0,0 +1,7 @@
+import time
+
+seconds = int(input("Set timer (in seconds): "))
+print("Timer Started...")
+
+time.sleep(seconds)
+print("⏰ Time's Up!")
---
+++ Temperature Converter (Celsius ↔ Fahrenheit)/Temperature.py
@@ -0,0 +1,3 @@
+C = float(input("Enter temperature in Celsius: "))
+F = (C * 9/5) + 32
+print("Fahrenheit:", F)
