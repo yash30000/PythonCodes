4906940353282a6c78a558c3ad6297628cebdff6
Yashraj Wakadkar <136210721+yash30000@users.noreply.github.com>
2026-01-13 04:51:28 -0800

    Update quiz questions to include colons

--- Quiz Game/quize.py
+++ Quiz Game/quize.py
@@ -1,10 +1,10 @@
 score = 0
 
-q = input("Q1. Python is a programming language? (yes/no): ")
+q = input("Q1. Python is a programming language? (yes/no):- ")
 if q.lower() == "yes":
     score += 1
 
-q = input("Q2. 5+5 = 10? (yes/no): ")
+q = input("Q2. 5+5 = 10? (yes/no):- ")
 if q.lower() == "yes":
     score += 1
 
